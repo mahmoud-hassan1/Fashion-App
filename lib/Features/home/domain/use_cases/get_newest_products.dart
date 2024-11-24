bad880b6562e1573968cbc71e5c161b2057b4ed6
mahmoud <mahmoudhassan10191019@gmail.com>
2024-11-24 22:10:40 +0200

    newest product use case

---
+++ lib/Features/home/domain/use_cases/get_newest_products.dart
@@ -0,0 +1,18 @@
+import 'package:online_shopping/Features/home/domain/entities/product_entity.dart';
+import 'package:online_shopping/Features/home/domain/repos/home_repo.dart';
+
+class GetNewestProducts {
+  final HomeRepo repository;
+
+  GetNewestProducts(this.repository);
+
+  Future<List<Product>> call() async {
+    try {
+      final productModels = await repository.getNewestProducts();
+      return productModels.map((model) => model.toEntity()).toList();
+    } catch (e) {
+      throw Exception('Failed to load newest products: $e'); 
+    }
+  }
+}
+
