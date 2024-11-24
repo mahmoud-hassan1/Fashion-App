a17339cf88e3cb10d4aa357fc12ecb9cff2b0f16
mahmoud <mahmoudhassan10191019@gmail.com>
2024-11-24 22:10:56 +0200

    get product by category use case

---
+++ lib/Features/home/domain/use_cases/get_products_by_cat.dart
@@ -0,0 +1,17 @@
+// lib/Features/home/domain/usecases/get_products_by_category.dart
+import 'package:online_shopping/Features/home/domain/entities/product_entity.dart';
+import 'package:online_shopping/Features/home/domain/repos/home_repo.dart';
+class GetProductsByCategory {
+  final HomeRepo repository;
+
+  GetProductsByCategory(this.repository);
+
+  Future<List<Product>> call(List<String> category) async {
+    try {
+      final productModels = await repository.getProductsByCategory(category);
+      return productModels.map((model) => model.toEntity()).toList();
+    } catch (e) {
+      throw Exception('Failed to load products: $e'); 
+    }
+  }
+}
