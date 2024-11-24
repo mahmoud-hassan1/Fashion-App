afa9f416d1ef8c73f38daf81b771450c5c6a09f8
mahmoud <mahmoudhassan10191019@gmail.com>
2024-11-24 22:11:36 +0200

    Products on sale cubit

---
+++ lib/Features/home/presentation/cubits/sale_cubit/sale_cubit.dart
@@ -0,0 +1,21 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:online_shopping/Features/home/domain/entities/product_entity.dart';
+import 'package:online_shopping/Features/home/domain/use_cases/get_sale_products.dart';
+
+part 'sale_state.dart';
+
+class SaleCubit extends Cubit<SaleState> {
+  final GetSaleProducts getSaleProducts;
+  SaleCubit({required this.getSaleProducts}) : super(SaleInitial());
+
+  Future<void> getProductsOnSale() async {
+    emit(SaleLoading());
+    try {
+      final products = await getSaleProducts.call();
+      emit(SaleSuccess(products: products));
+    } catch (e) {
+      emit(SaleFailed(message: 'Failed to load sale products: $e'));
+    }
+  }
+}
