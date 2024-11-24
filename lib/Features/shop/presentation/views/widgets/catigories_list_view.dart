e3b83073114a167de1f67571817cbff06088862e
mahmoud <mahmoudhassan10191019@gmail.com>
2024-11-24 22:12:22 +0200

    catigories list view

---
+++ lib/Features/shop/presentation/views/widgets/catigories_list_view.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:online_shopping/core/utiles/constants.dart';
+import 'package:online_shopping/core/utiles/font.dart';
+
+class CatigoriesListView extends StatelessWidget {
+  const CatigoriesListView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+      
+    delegate: SliverChildBuilderDelegate(
+      (BuildContext context, int index) {
+        return AspectRatio(
+          aspectRatio: 3/1,
+          child: Container(
+            width: double.infinity,
+            margin: const EdgeInsets.symmetric(horizontal: 16,vertical: 8),
+            decoration: BoxDecoration(
+    
+              color:  Colors.white,
+              borderRadius: BorderRadius.circular(8),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.shade300,
+                  blurRadius: 2,
+                ),
+              ],
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Row(
+                    children: [
+                      const SizedBox(width: 24,),
+                      Text(
+                        kCategoryList[index].first,
+                      style: FontStyles.kMediumTextStyle(context).copyWith(
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Image.asset(kCategoryList[index].last,fit: BoxFit.fitWidth,),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+      childCount: kCategoryList.length,
+    ),
+    );
+  }
+}
+
