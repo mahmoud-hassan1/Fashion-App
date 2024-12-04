ab58787fb0431ccf397162a20846dd5e74614332
Zeyad Ali <z01551153743@gmail.com>
2024-12-04 22:44:26 +0200

    craete my bag view

--- lib/Features/bag/presentation/views/widgets/bag_view_body.dart
+++ lib/Features/bag/presentation/views/widgets/bag_view_body.dart
@@ -1,10 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:online_shopping/Features/auth/presentation/views/widgets/custtom_button.dart';
+import 'package:online_shopping/core/utiles/styles.dart';
 
 class BagViewBody extends StatelessWidget {
   const BagViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          Text("My Bag", style: Styles.kMediumTextStyle(context).copyWith(fontSize: 34)),
+          ListView.builder(
+            itemCount: 5,
+            itemBuilder: (BuildContext context, int index) {
+              return const Column(
+                children: [
+                  SizedBox(height: 24),
+                  MyBagItem(),
+                ],
+              );
+            },
+          ),
+          CustomButton(height: 50, label: "", onTap: () {}),
+        ],
+      ),
+    );
+  }
+}
+
+class MyBagItem extends StatelessWidget {
+  const MyBagItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(height: 100, width: 100, color: Colors.amber),
+        const Padding(
+          padding: EdgeInsets.all(12),
+          child: Row(
+            children: [
+              Column(
+                children: [
+                  Text("Pullover"),
+                ],
+              ),
+              Column(),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
